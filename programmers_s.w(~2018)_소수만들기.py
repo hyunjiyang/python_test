8a2a1b7a92287403a27457a2a76ba42b4f48eb0e
Hyun-JI Yang <70367386+hyunjiyang@users.noreply.github.com>
2021-10-16 20:06:53 +0900

    Update programmers_s.w(~2018)_소수만들기.py

--- programmers_s.w(~2018)_소수만들기.py
+++ programmers_s.w(~2018)_소수만들기.py
@@ -1,18 +1,14 @@
+#try1 : 중첩 for문을 없애는 방법은 없나?
 import itertools
 
 def solution(nums):
-    answer = []
-    
+    answer = 0
     num = list(map(sum,itertools.combinations(nums,3)))
     for i in num:
-        n=2
-        while n < i :
+        for n in range(2,i):
             if i%n == 0:
                 break
-            
-                
-            
-        
-        #answer+=1
+            if n == i-1:
+                answer+=1
         
     return answer
