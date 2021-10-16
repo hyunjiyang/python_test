bb7afc0b3f96c515b461b2da43fe4a154c3d0768
Hyun-JI Yang <70367386+hyunjiyang@users.noreply.github.com>
2021-10-16 23:07:42 +0900

    Update programmers_s.w(~2018)_소수만들기.py
    compact ver

--- programmers_s.w(~2018)_소수만들기.py
+++ programmers_s.w(~2018)_소수만들기.py
@@ -1,9 +1,9 @@
 #try1 : 중첩 for문을 없애는 방법은 없나?
-import itertools
+from itertools import combinations as cb
 
 def solution(nums):
     answer = 0
-    num = list(map(sum,itertools.combinations(nums,3)))
+    num = list(map(sum,cb(nums,3)))
     for i in num:
         for n in range(2,i):
             if i%n == 0:
