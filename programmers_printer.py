9f880fb72f71ee24954e72286a8e7569b37590e7
Hyun-JI Yang <70367386+hyunjiyang@users.noreply.github.com>
2021-10-20 17:58:48 +0900

    Update programmers_printer.py

--- programmers_printer.py
+++ programmers_printer.py
@@ -1,3 +1,4 @@
+#try 1 : rotate 사용
 from collections import deque
 def solution(priorities, location):
     answer = 0
@@ -19,4 +20,20 @@ def solution(priorities, location):
                 answer += 1
         else : 
             deq.rotate(-1)
+    return answer
+
+#try2 :enumerate = 인덱스값 튜플로 함께 생성 , any() = 조건이 하나라도 해당되면 true출력
+
+def solution(priorities, location):
+    answer = 0
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue) :
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                break
     return answer
