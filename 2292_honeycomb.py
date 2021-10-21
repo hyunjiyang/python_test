0fed3be95d7b89ce953e00fdcd223192a015f955
Hyun-JI Yang <70367386+hyunjiyang@users.noreply.github.com>
2021-10-22 02:55:39 +0900

    Update 2292_honeycomb.py

--- 2292_honeycomb.py
+++ 2292_honeycomb.py
@@ -1,9 +1,20 @@
+#try1
 n = int(input())
 a = 0
 i = 1
 
 while n-1 > a :
     a += 6*i
+    i += 1
+
+print(i)
+
+#try2
+n = int(input())
+i = 1
+
+while i > 1 :
+    n -= 6*i
     i += 1
 
 print(i)
