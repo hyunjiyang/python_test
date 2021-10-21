de81ebb835a791858789f174ff2be4cada90e1a4
Hyun-JI Yang <70367386+hyunjiyang@users.noreply.github.com>
2021-10-21 23:55:57 +0900

    Update programmers_네트워크.py

--- programmers_네트워크.py
+++ programmers_네트워크.py
@@ -14,3 +14,26 @@ def solution(n, computers):
             answer +=1
 
     return answer
+
+#BFS
+from collections import deque
+
+def solution(n, computers):
+    answer = 0
+    def bfs(i):
+        deq = deque()
+        deq.append(i)
+        while deq:
+            i = deq.popleft()
+            visited[i] = 1
+            for a in range(n):
+                if computers[i][a] == 1 and visited[a] == 0 :
+                    deq.append(a)
+                
+    visited = [0 for i in range(n)]
+    for i in range(n):
+        if visited[i] == 0 :
+            bfs(i)
+            answer +=1
+
+    return answer
