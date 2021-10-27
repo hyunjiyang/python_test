bcc527c851b918fdf5f663b7e3aa44c5b417a194
Hyun-JI Yang <70367386+hyunjiyang@users.noreply.github.com>
2021-10-28 02:47:32 +0900

    Update 1697_hideandseek.py

--- 1697_hideandseek.py
+++ 1697_hideandseek.py
@@ -23,6 +23,35 @@ def bfs(n):
 
 print(bfs(n))
 
+#bfs 최적화
+from collections import deque
+n,k = map(int,input().split())
+deq = deque()
+deq.append([n,0])
+visited = [0]*100001
+
+def bfs(n):
+    while deq:
+        v = deq.popleft()
+        x, count = v[0],v[1]
+        if visited[x]==0:
+            visited[x]=1
+            if x == k:
+                return count
+            count += 1
+            if 0<=x-1:
+                deq.append([x-1,count])
+            if x+1<=k+1: #끝까지 갈 필요x k+1이 최대
+                deq.append([x+1,count])
+            if x*2<=k+1: #x*2가 k+1이상이면 작은게 이득.
+                deq.append([x*2,count])
+    return count
+
+if n>=k: #최적화! 이 경우 뒤로 한칸씩 가는 방법 뿐
+    print(n-k) 
+else:
+    print(bfs(n))
+
 # 재귀
 n, k = map(int,input().split())
 
