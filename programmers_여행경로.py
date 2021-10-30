6bccdca8a7aeba97ebef41dae64c09b0cc8213b3
Hyun-JI Yang <70367386+hyunjiyang@users.noreply.github.com>
2021-10-30 23:35:29 +0900

    Update programmers_여행경로.py

--- programmers_여행경로.py
+++ programmers_여행경로.py
@@ -23,7 +23,7 @@ def solution(tickets):
     
     return dfs("ICN")
 
-#try2 : visited를 안쓰고 싶다!!! -> testcase 실패...
+#try2 : visited를 안쓰고 싶다!!! -> testcase 실패... => 티켓의 KEY가 하나일 경우 문제 발생!!
 from collections import defaultdict
 
 def solution(tickets):
@@ -42,7 +42,7 @@ def solution(tickets):
             if routes[key] != []:
                 stack.append(routes[key].pop(0))
             else:
-                break
+                break # KEY가 1개면 티켓 1장만 사용하고 BREAK됨
         return stack[::]
     
     return dfs("ICN")
