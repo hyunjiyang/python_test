dcec7158725fb51120e1481f8ebb3a096bf73025
Hyun-JI Yang <70367386+hyunjiyang@users.noreply.github.com>
2021-11-01 00:10:19 +0900

    Update 1260_dfs_bfs_best.py

--- 1260_dfs_bfs_best.py
+++ 1260_dfs_bfs_best.py
@@ -1,25 +1,26 @@
 from collections import deque
-
 n,m,v = map(int,input().split())
 
-#구조 틀 만들기
+#그래프 [start][end]만들기
 graph = [[] for _ in range(n+1)] # 1*n+1 배열
-visited_dfs = [0]*(n+1)
-visited_bfs = [0]*(n+1)
-deq = deque([v])
-dfs_print = []
-bfs_print = []
-
-#그래프 채우기
-for i in range(m):
+for _ in range(m):
     start, end = map(int, input().split())
     graph[start].append(end) #[],[123],[468]
     graph[end].append(start) #양방향
 
-#함수만들기
+#dfs준비
+visited_dfs = [0]*(n+1)
+answer_dfs = []
+
+#bfs준비
+visited_bfs = [0]*(n+1)
+deq = deque([v])
+answer_bfs = []
+
+
 def DFS(v):
     visited_dfs[v] = 1
-    dfs_print.append(v)
+    answer_dfs.append(v)
     graph[v].sort() #순서대로 각 행 정렬
     for i in graph[v] :
         if visited_dfs[i] == 0 :
@@ -30,7 +31,7 @@ def BFS(v):
         a = deq.popleft()
         if visited_bfs[a] == 0:
             visited_bfs[a] = 1
-            bfs_print.append(a)
+            answer_bfs.append(a)
             deq.extend(graph[a])
 
 #실행함수
